2dcaca354dab780e77110945a7a6b411b5d11d5d
Krishna Ruparelia <krishna.ruparelia@profound.works>
2024-07-29 18:03:45 +0530

    pagination logic done in backend

--- server/routes/products.js
+++ server/routes/products.js
@@ -4,13 +4,25 @@ import Product from "../models/product.js";
 const productRoutes = express.Router();
 
 const getProducts = async (req, res) => {
+  const page = parseInt(req.params.page); // 1,2.3
+  const perPage = parseInt(req.params.perPage); // 10
   const products = await Product.find({});
-  res.json({
-    products,
-    pagination: {},
-  });
+
+  if (page && perPage) {
+    const totalPages = Math.ceil(products.length / perPage);
+    const startIndex = (page - 1) * perPage;
+    const endIndex = startIndex + perPage;
+    const paginatedProducts = products.slice(startIndex, endIndex);
+    res.json({
+      products: paginatedProducts,
+      pagination: { currentPage: page, totalPages },
+    });
+  } else {
+    res.json({ products, pagination: {} });
+  }
 };
 
+productRoutes.route("/:page/:perPage").get(getProducts);
 productRoutes.route("/").get(getProducts);
 
 export default productRoutes;
