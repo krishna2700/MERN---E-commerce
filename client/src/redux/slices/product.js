eed87f8c186e2c0edc0262fa6f4c7deeffe97d5f
Krishna Ruparelia <krishna.ruparelia@profound.works>
2024-07-29 16:16:39 +0530

    Product Slice created for redux setup

---
+++ client/src/redux/slices/product.js
@@ -0,0 +1,53 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const initialState = {
+  loading: false,
+  error: null,
+  products: [],
+  product: null,
+  pagination: {},
+  favoritesToggled: true,
+};
+
+export const productSlice = createSlice({
+  name: "product",
+  initialState,
+  reducers: {
+    setLoading: (state) => {
+      state.loading = true;
+    },
+    setError: (state, { payload }) => {
+      state.loading = false;
+      state.error = payload;
+    },
+    setProducts: (state, { payload }) => {
+      state.loading = false;
+      state.error = null;
+      state.products = payload;
+    },
+    setPagination: (state, { payload }) => {
+      state.loading = false;
+      state.error = null;
+      state.pagination = payload;
+    },
+    setProduct: (state, action) => {
+      state.product = action.payload;
+    },
+    setFavoritesToggled: (state, action) => {
+      state.favoritesToggled = action.payload;
+    },
+  },
+});
+
+export const {
+  setLoading,
+  setError,
+  setProducts,
+  setPagination,
+  setProduct,
+  setFavoritesToggled,
+} = productSlice.actions;
+
+export default productSlice.reducer;
+
+export const productSelector = (state) => state.product;
