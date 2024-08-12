e0c85f762f9d42f1a52534de0e6a641d99aeecc6
Krishna Ruparelia <krishna.ruparelia@profound.works>
2024-08-12 13:24:14 +0530

    Cart action done and added in store

---
+++ client/src/redux/actions/cartAction.js
@@ -0,0 +1,59 @@
+import axios from "axios";
+import {
+  setLoading,
+  setError,
+  setCartItems,
+  clearCart,
+  cartItemRemoval,
+  setShippingCosts,
+  setSubtotal,
+} from "../slices/cart";
+
+export const addCartItems = (id, qty) => async (dispatch) => {
+  dispatch(setLoading(true));
+  try {
+    const { data } = await axios.get(`/api/products/${id}`);
+    const itemToAdd = {
+      id: data._id,
+      name: data.name,
+      subtitle: data.subtitle,
+      image: data.images[0],
+      price: data.price,
+      stock: data.stock,
+      brand: data.brand,
+      qty,
+      stripeId: data.stripeId,
+    };
+    dispatch(setCartItems(itemToAdd));
+    dispatch(setCartItems({ product: data._id, qty }));
+  } catch (err) {
+    dispatch(
+      setError(
+        err.response && err.response.data.message
+          ? err.response.data.message
+          : err.message
+          ? err.message
+          : "An unexpected error has occurred"
+      )
+    );
+  }
+};
+
+export const removeCartItem = (id) => async (dispatch) => {
+  dispatch(setLoading(true));
+  dispatch(cartItemRemoval(id));
+  dispatch(clearCart());
+};
+
+export const clearCartItems = () => async (dispatch) => {
+  dispatch(clearCart());
+  dispatch(setLoading(true));
+};
+
+export const setShipping = (value) => async (dispatch) => {
+  dispatch(setShippingCosts(value));
+};
+
+export const restCart = () => async (dispatch) => {
+  dispatch(clearCart());
+};
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,10 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import productReducer from "./slices/product";
+import cart from "./slices/cart";
 
 const reducer = combineReducers({
   product: productReducer,
+  cart,
 });
 
 const store = configureStore({
