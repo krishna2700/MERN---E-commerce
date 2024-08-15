f7aa02a951751ff19d6424db05f611529b3aba38
Krishna <krishnaruparelia0207@gmail.com>
2024-08-15 11:02:37 +0530

    logi and register api created

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ import cors from "cors";
 import connectRoDatabase from "./db.js";
 // Routes
 import productRoutes from "./routes/productRoutes.js";
-// import userRoutes from "./routes/userRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 connectRoDatabase();
@@ -13,11 +13,11 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/api/products", productRoutes);
-// app.use("/api/users", userRoutes);
+app.use("/api/users", userRoutes);
 
 const PORT = 3690;
 
-app.get("./", (req, res) => {
+app.get("/", (req, res) => {
   res.send("Api is running");
 });
 
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -39,6 +39,37 @@ const loginUser = asyncHandler(async (req, res) => {
 });
 
 // * register
+const register = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+  const userExist = await User.findOne({ email });
+
+  if (userExist) {
+    res.status(400).send("User already exists");
+  }
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+  const newToken = generateToken(user._id);
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      googleImage: user.googleImage,
+      googleId: user.googleId,
+      isAdmin: user.isAdmin,
+      token: newToken,
+      active: user.active,
+      firstLogin: user.firstLogin,
+      createdAt: user.createdAt,
+    });
+  } else {
+    res.status(400).send("We Could not register your account");
+    throw new Error("Invalid user data");
+  }
+});
 
 // * verificationEmail
 
@@ -47,5 +78,6 @@ const loginUser = asyncHandler(async (req, res) => {
 // * password reset
 
 userRoutes.route("/login").post(loginUser);
+userRoutes.route("/register").post(register);
 
 export default userRoutes;
